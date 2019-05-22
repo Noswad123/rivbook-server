46fea4b62349968ccdf55248a7eba1cacf3475cd
Jamal Dawson <jdawson@6river.com>
2019-05-22 11:28:56 -0400

    fix: add / path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+ @Get('')
+  getHello(): string {
+    return 'Greetings';
+  }
   @Get('channels')
-  getHello(): string[] {
+  getChannels(): string[] {
     return ['starman_Channel', 'dev-quest', 'gen_industry', 'gen_movies'];
   }
 }
